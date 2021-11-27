af8434f6eed0cf9c759e69fd0e8ca9a3b163bce7
Sean <seanlengws@gmail.com>
2021-11-27 22:15:48 +0800

    [Added] Client files

---
+++ client/main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST", "GET"])
+def home():
+    if request.method == 'POST':
+        username = request.form.get('username')  # access the data inside 
+        password = request.form.get('password')
+
+        if username == 'root' and password == 'pass':      
+            return render_template("login.html", username=username)
+        else:
+            return render_template("index.html")
+    return render_template("index.html")
+    
+
+@app.route("/<name>")
+def user(name):
+	return f"Hello {name}!"
+
+if __name__ == "__main__":
+	app.run()
